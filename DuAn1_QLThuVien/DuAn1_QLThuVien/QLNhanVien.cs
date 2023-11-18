5691010e89ff6cdb5998a6e6bef04a02874069c3
Hennry20 <145514814+Hennry20@users.noreply.github.com>
2023-11-18 08:23:33 +0700

    thêm form nhanvien + thong ke

---
+++ DuAn1_QLThuVien/DuAn1_QLThuVien/QLNhanVien.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLThuVien
+{
+    public partial class QLNhanVien : Form
+    {
+        public QLNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
