edb21859ff31ec23af95910ec4565d107a76f203
AnhChuongChovy <chuonggo51@gmail.com>
2023-11-28 08:28:34 +0700

    quay lai luc chua merge

--- DuAn1_QLThuVien/DuAn1_QLThuVien/DangNhap.Designer.cs
+++ DuAn1_QLThuVien/DuAn1_QLThuVien/DangNhap.Designer.cs
@@ -168,7 +168,11 @@ namespace DuAn1_QLThuVien
             this.checkBox1.AutoSize = true;
             this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
 <<<<<<< HEAD
+<<<<<<< HEAD
             this.checkBox1.Location = new System.Drawing.Point(26, 327);
+=======
+            this.checkBox1.Location = new System.Drawing.Point(24, 355);
+>>>>>>> parent of ca8bd31 (Sua Form DangNhap)
 =======
             this.checkBox1.Location = new System.Drawing.Point(24, 355);
 >>>>>>> parent of ca8bd31 (Sua Form DangNhap)
